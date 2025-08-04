af1e27c813fb90bd2c745efd66d964ff87c6596a
wolterbwambale <benplaan1@gmail.com>
2025-08-04 15:54:12 +0300

    feat(update herosection):ave worked on the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,14 +4,17 @@ function Hero() {
   return (
     <div className="text-white">
       <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto flex flex-col text-center justify-center">
-        <p className="text-[#00df9a] font-bold">TREKKING GORILLA </p>
-        <h1 className="md:text-7xl sm:6xl text-xl md:py-6 font-bold">
+        <p className="text-[#00df9a] font-bold py-2 font-sans text-center">
+          TREKKING GORILLA
+        </p>
+        <h1 className="md:text-6xl sm:6xl text-xl md:py-6 font-bold">
           GO TREKKING GORILLA
         </h1>
-        <div>
-          <p className="md:text-4xl sm:text-4xl text-xl font-semibold">
+        <div className="flex justify-center items-center text-center">
+          <p className="md:text-3xl sm:text-3xl text-xl font-semibold sm:text-center">
             Let us take you places trekking Gorillas in{" "}
             <ReactTyped
+              className="text-[#00df9a] font-serif font-bold text-center"
               strings={[
                 " Uganda ",
                 " Rwanda",
@@ -25,6 +28,12 @@ function Hero() {
             ></ReactTyped>
           </p>
         </div>
+        <p className="py-2 text-gray-500 font-bold md:5xl text-xl sm:text-4xl">
+          face to face with Gorilla
+        </p>
+        <button className="bg-[#00df9a] py-3 w-[200px]  mx-auto rounded-md my-6 text-black font-medium">
+          See More
+        </button>
       </div>
     </div>
   );
