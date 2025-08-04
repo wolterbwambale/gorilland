8d438287b26aab4d781baf70be24693aafe0dbb2
wolterbwambale <benplaan1@gmail.com>
2025-08-04 09:39:35 +0300

    feat(add hero section):implemnent the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,31 @@
 import React from "react";
-
+import { ReactTyped } from "react-typed";
 function Hero() {
   return (
-    <div>
-      <h1 className="text-white">Hero</h1>
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto flex flex-col text-center justify-center">
+        <p className="text-[#00df9a] font-bold">TREKKING GORILLA </p>
+        <h1 className="md:text-7xl sm:6xl text-xl md:py-6 font-bold">
+          GO TREKKING GORILLA
+        </h1>
+        <div>
+          <p className="md:text-4xl sm:text-4xl text-xl font-semibold">
+            Let us take you places trekking Gorillas in{" "}
+            <ReactTyped
+              strings={[
+                " Uganda ",
+                " Rwanda",
+                " Tanzania",
+                " Zambabwe",
+                " South Africa",
+              ]}
+              typeSpeed={140}
+              backSpeed={150}
+              loop
+            ></ReactTyped>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
