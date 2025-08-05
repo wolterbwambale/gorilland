462c8f18ec3b9ea3c2d75f1fb23bf2d3a0c7ccd4
wolterbwambale <benplaan1@gmail.com>
2025-08-05 07:52:15 +0300

    added  images f

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Analytics from "./components/Analytics";
+import Newsletter from "./components/Newsletter";
+import Cards from "./components/Cards";
 function App() {
   return (
     <div className="">
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
+      <Cards />
     </div>
   );
 }
